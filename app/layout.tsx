3a19dcf5bcf2a45d05876439829a64df7e60b2d3
Sanjana LR <lrsanju07@gmail.com>
2026-07-14 11:41:02 +0530

    setup app,file and folder structure,routing and auth pages

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import type { Metadata } from "next";
+import { Mona_Sans } from "next/font/google";
+import "./globals.css";
+import { Toaster } from "sonner";
+
+const monaSans = Mona_Sans({
+  variable: "--font-mona-sans",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "AI Mock Interview",
+  description: "AI Interview Platform",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en" className="dark">
+      <body className={`${monaSans.className} antialised pattern` }>
+        {children}
+        <Toaster/>
+      </body>
+    </html>
+  );
+}
